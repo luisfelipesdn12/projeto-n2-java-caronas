acb942d3d66edf6b01a052e2d3ad44806259f272
Luis Felipe <luisfelipesdn12@gmail.com>
2024-05-18 11:01:04 -0300

    feat: Add Menu

---
+++ Menu.java
@@ -0,0 +1,16 @@
+public class Menu {
+    void mostrarCabecalho() {
+        System.out.println(
+            "\n CarONE-M: Viagens compartilhadas\n\n" +
+            "           ______\n" +
+            "           /|_||_\\`.__\n" +
+            "          (   _    _ _\\\n" +
+            "          =`-(_)--(_)-'\n\n" +
+            "     Luis - Marcos - Matheus\n"
+        );
+    }
+
+    void limparTerminal() {
+        System.out.println(System.lineSeparator().repeat(50));
+    }
+}
