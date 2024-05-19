6225fc0eb02f71880c1a7c51e7e85f85f06595a9
Luis Felipe <luisfelipesdn12@gmail.com>
2024-05-19 00:27:34 -0300

    remove system.out

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ public class Main {
         Repositorio repositorio = new Repositorio();
         Menu menu = new Menu(repositorio);
 
-        System.out.println(repositorio.getUsuarioPeloLogin("anasilva").isPresent());
-
         while (menu.usuarioLogado.isEmpty()) {
             menu.mostrarInicio();
         }
